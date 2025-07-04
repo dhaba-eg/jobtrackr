d3509c0fb491b300f9ec0ca0fb831be6764039df
dhanush <dhaba@eg.dk>
2025-07-04 14:42:10 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { Card } from '@/components/ui/card'
+// Add other components you installed
+</script>
+
+<template>
+  <div class="p-8 space-y-4">
+    <Button>Test Button</Button>
+    <Card class="p-4">Test Card</Card>
+  </div>
+</template>
